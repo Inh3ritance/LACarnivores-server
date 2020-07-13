ea817a9883192bb6dee16c9eb4cf099fa5f7f6d8
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-07-12 23:10:04 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -9,18 +9,11 @@ const { uuid } = require('uuidv4');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 
-const whitelist = ['https://www.lacarnivores.com', 'https://www.lacarnivores.com/Checkout']
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(corsOptions);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://www.lacarnivores.com/*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 router.get('/Hello', (req, res) => {
     res.json({
