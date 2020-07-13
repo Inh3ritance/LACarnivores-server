ca333e28f367b7afcd8eeb3a83d6a47fcebc7c60
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-07-12 23:22:27 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -14,6 +14,8 @@ app.use(function(req, res, next) {
     var origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
          res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
     }
     return next();
   });
