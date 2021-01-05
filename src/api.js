6e45055e2ba975ff173156441f94750fe751b724
Inh3ritance <herreralaces@gmail.com>
2021-01-05 12:28:00 -0800

    maybe...

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     const allowedOrigins = ['https://www.lacarnivores.com', 'https://www.lacarnivores.com/Checkout', 'https://www.lacarnivores.com/Contact'];
     const origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
-         res.header('Access-Control-Allow-Origin', origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
          res.header('Access-Control-Allow-Methods','POST, GET');
 		 res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 		 res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
