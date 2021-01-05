2691246831690b0565621b144ea37ede0ea17b75
Inh3ritance <herreralaces@gmail.com>
2021-01-04 18:57:35 -0800

    last time hopefully

--- src/api.js
+++ src/api.js
@@ -301,6 +301,7 @@ router.post("/verify", (req, res) => {
     var VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_KEY}&response=${req.body['g-recaptcha-response']}`;
     return fetch(VERIFY_URL, { method: 'POST' })
     .then(res => res.json())
+    .catch(err => res.send(err))
     .then(json => res.send(json))
     .catch(err => console.log(err));
 });
