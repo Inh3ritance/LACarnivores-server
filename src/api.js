6f16896aa6fd035d87ea5e89147be4bf74a33948
Inh3ritance <herreralaces@gmail.com>
2021-02-16 19:04:22 -0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -261,7 +261,7 @@ router.get('/prices', async (req, res) => {
 });
 
 router.options('/verify', cors(config));
-router.post('/verify', async (req, res) => {
+router.post('/verify', cors(config), async (req, res) => {
     var VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_KEY}&response=${req.body.response}`;
     return await fetch(VERIFY_URL, { 
         method: 'POST',
