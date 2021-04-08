d9d0cc8f338bfa6905fda1d3e7318e1d424d67a7
Inh3ritance <herreralaces@gmail.com>
2021-04-07 18:42:53 -0700

    revert

--- src/funct/easypostFunctions.js
+++ src/funct/easypostFunctions.js
@@ -1,4 +1,4 @@
-const Easypost = require('@easypost/api/src/easypost');
+const Easypost = require('@easypost/api');
 const easypost = new Easypost(process.env.EASYPOST);
 const nodemailer = require('nodemailer'); 
 const email_config = require('../configs/emailConfig');
